3d1cc9b06eec6ce675b3ff771f29e6ec1f484d3e
WaterBleu <jeff7746@hotmail.com>
2015-06-30 11:36:38 -0700

    Initial Commit

---
+++ MathGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MathGame
+//
+//  Created by Jeff Huang on 2015-06-30.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
